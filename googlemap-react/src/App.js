7be99e9e4bf796805d04a8355c5a81d426ba721b
Rachael Njeri <rachael.njeri@andela.com>
2018-08-12 14:37:33 +0300

    Add marker and Infowindow

--- googlemap-react/src/App.js
+++ googlemap-react/src/App.js
@@ -10,17 +10,46 @@ const style = {
 };
 
 export class MapContainer extends Component {
+  state = { showingInfoWindow: false, activeMarker: {}, selectedPlace: {} };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
       <Map
         google={this.props.google}
         zoom={14}
         style={style}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233
-        }}
-      />
+        initialCenter={{ lat: -1.2884, lng: 36.8233 }}
+        onClick={this.onMapClicked}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
