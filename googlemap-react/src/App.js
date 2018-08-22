6962510f7770b049070968ddc2f57fa2fa9212e3
Rachael Njeri <rachael.njeri@andela.com>
2018-08-22 07:08:10 +0300

    remove unused imports
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- googlemap-react/src/App.js
+++ googlemap-react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 import CurrentLocation from './Map';
 
